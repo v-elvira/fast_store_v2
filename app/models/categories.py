7fe08cfe80d19592cb8d5050886c4f0ea280910d
elya <vavina.elvira@gmail.com>
2025-11-11 11:11:55 +0300

    add ForeignKeys and relationships

--- app/models/categories.py
+++ app/models/categories.py
@@ -1,8 +1,12 @@
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 # sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../..'))  # add if startup problem
 from app.database import Base
+
+# from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+#     from app.models.products import Product
 
 
 class Category(Base):
@@ -11,6 +15,10 @@ class Category(Base):
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    parent_id: Mapped[int | None] = mapped_column(ForeignKey("categories.id"), nullable=True)
+    products: Mapped[list["Product"]] = relationship("Product", back_populates="category")
+    parent: Mapped["Category|None"] = relationship("Category", back_populates="children", remote_side="Category.id")
+    children: Mapped[list["Category"]] = relationship("Category", back_populates="parent")
 
 
 if __name__ == "__main__":
@@ -21,9 +29,11 @@ if __name__ == "__main__":
     print(CreateTable(Category.__table__))
     '''
     CREATE TABLE categories (
-	    id INTEGER NOT NULL, 
-	    name VARCHAR(50) NOT NULL,
-	    is_active BOOLEAN NOT NULL, 
-	    PRIMARY KEY (id)
+        id INTEGER NOT NULL, 
+        name VARCHAR(50) NOT NULL, 
+        is_active BOOLEAN NOT NULL, 
+        parent_id INTEGER, 
+        PRIMARY KEY (id), 
+        FOREIGN KEY(parent_id) REFERENCES categories (id)
     )
     '''
--- app/models/products.py
+++ app/models/products.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
-from sqlalchemy import String, Boolean, Integer, Numeric
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Integer, Numeric, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
 
@@ -15,3 +15,6 @@ class Product(Base):
     image_url: Mapped[str | None] = mapped_column(String(200), nullable=True)
     stock: Mapped[int] = mapped_column(Integer, nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), nullable=False)
+
+    category: Mapped["Category"] = relationship(back_populates="products")
